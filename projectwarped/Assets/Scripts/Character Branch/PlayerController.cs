262d25488e825512b0d930844e470fdfbf7c26c2
Ryan Hammontree <HammontreeRyan@gmail.com>
2014-11-10 20:46:53 -0600

    added Test Player
    Player isn't colliding

---
+++ projectwarped/Assets/Scripts/Character Branch/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController: MonoBehaviour {
+
+	//animator
+	//private Animator animator;
+
+	// standard movement values
+	public float maxSpeed = 10f;
+	public bool facingRight = false;
+
+	//Damage Responce
+	public float takenDamage = 0.2f;
+
+	
+	// Use this for initialization
+	void Start()
+	{
+		Flip ();
+		//animator = GetComponent<Animator>();
+	}
+	
+	// FixedUpdate updates at regular intervals
+	// For physics calculations
+	void FixedUpdate()
+	{
+		
+	}
+
+	// Update updates at irregular intervals for maximum accuracy
+	void Update()
+	{
+
+		// enables sprite movement
+		float Hmove = Input.GetAxis ("Horizontal");
+		float Vmove = Input.GetAxis ("Vertical");
+		rigidbody2D.velocity= new Vector2 (Hmove * maxSpeed, Vmove * maxSpeed);
+		//animator.SetFloat ("Speed", Mathf.Abs (move));
+		
+		//Flips Sprite
+		if (Hmove > 0 && !facingRight) 
+			Flip ();
+		else if (Hmove < 0 && facingRight) 
+			Flip ();
+		
+	}
+	
+	// Flips Sprite
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector2 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+
+	public IEnumerator TakenDamage()
+	{
+		renderer.enabled = false;
+		yield return new WaitForSeconds(takenDamage);
+		renderer.enabled = true;
+		yield return new WaitForSeconds(takenDamage);
+		renderer.enabled = false;
+		yield return new WaitForSeconds(takenDamage);
+		renderer.enabled = true;
+		yield return new WaitForSeconds(takenDamage);
+		renderer.enabled = false;
+		yield return new WaitForSeconds(takenDamage);
+		renderer.enabled = true;
+		yield return new WaitForSeconds(takenDamage);
+		renderer.enabled = false;
+		yield return new WaitForSeconds(takenDamage);
+		renderer.enabled = true;
+		yield return new WaitForSeconds(takenDamage);
+
+	}
+}
