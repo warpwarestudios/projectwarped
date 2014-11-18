80e76a0e40dc885f2b5b05c1d8efe508971d7c91
Ryan Hammontree <HammontreeRyan@gmail.com>
2014-11-18 12:14:27 -0600

    BuildScript

---
+++ projectwarped/Assets/Scripts/BuildScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+public class BuildScript{
+
+	static void BuildWindows64Player()
+		{
+		string buildTarget = System.Environment.GetEnvironmentVariable ("C:\\Users\\Administrator\\Dropbox\\Public\\Project Warped");
+				if (buildTarget == null || buildTarget.Length == 0) {
+						throw new Exception ("UNITY_BUILD_TARGET -system property not defined, aborting.");
+				}
+			
+				string[] scenes = { "Assets/GameLobby.unity", "Assets/GameSetting.unity", "Assets/MainGame.unity", "Assets/MainMenu.unity" };
+			
+				string error = BuildPipeline.BuildPlayer (scenes, buildTarget, BuildTarget.StandaloneWindows64, BuildOptions.None);
+			
+				if (error != null && error.Length > 0) {
+						throw new Exception ("Build failed: " + error);
+				}
+		}
+
+	}
+
