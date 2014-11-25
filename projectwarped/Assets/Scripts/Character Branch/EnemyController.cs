0f934b8fb8c35fa67442edfc56b9d52f79b38a94
Steven Burke <tsunayoshi1234@gmail.com>
2014-11-24 21:55:09 -0600

    Added Enemy Movement

--- projectwarped/Assets/Scripts/Character Branch/EnemyController.cs
+++ projectwarped/Assets/Scripts/Character Branch/EnemyController.cs
@@ -3,13 +3,118 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+	//animator
+	//private Animator animator;
+	
+	// standard movement values
+	public float maxSpeed = 50f;
+	public bool facingRight = false;
+	private float Hmove;
+	private float Vmove;
+	public float delay = 5f;
+	private float overTime;
+	private bool movingUp = true;
+	private bool movingRight = false;
+	private bool movingDown = false;
+	private bool movingLeft = false;
 	// Use this for initialization
-	void Start () {
-		this.rigidbody2D.fixedAngle = true;
+	void Start()
+	{
+		Flip ();
+		//animator = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	// FixedUpdate updates at regular intervals
+	// For physics calculations
+	void FixedUpdate()
+	{
+		
+	}
 	
+	// Update updates at irregular intervals for maximum accuracy
+	void Update()
+	{
+		// enables sprite movement
+		MovementChange ();
+		rigidbody2D.velocity = new Vector2 (Hmove * maxSpeed, Vmove * maxSpeed);
+		//animator.SetFloat ("Speed", Mathf.Abs (move));
+		
+		//Flips Sprite
+		if (Hmove > 0 && !facingRight) 
+			Flip ();
+		else if (Hmove < 0 && facingRight) 
+			Flip ();
+		
+	}
+	
+	void MovementChange()
+	{
+		if (Time.time >= overTime) 
+		{
+			if (movingUp)
+			{
+				MoveUp();
+				movingUp = false;
+				movingRight = true;
+				MovementChange();
+			}
+			else if (movingRight)
+			{
+				MoveRight();
+				movingRight = false;
+				movingDown = true;
+				MovementChange();
+			}
+			else if (movingDown)
+			{
+				MoveDown();
+				movingDown = false;
+				movingLeft = true;
+				MovementChange();
+			}
+			else if (movingLeft)
+			{
+				MoveLeft();
+				movingLeft = false;
+				movingUp = true;
+				MovementChange();
+			}
+		}
+	}
+	// Flips Sprite
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector2 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+	
+	void MoveRight()
+	{
+		Hmove = Time.deltaTime;
+		Vmove = 0;
+		overTime = Time.time + delay;
+	}
+	
+	void MoveLeft()
+	{
+		Hmove = -Time.deltaTime;
+		Vmove = 0;
+		overTime = Time.time + delay;
+	}
+	
+	void MoveUp()
+	{
+		Hmove = 0;
+		Vmove = Time.deltaTime;
+		overTime = Time.time + delay;
+	}
+	
+	void MoveDown()
+	{
+		Hmove = 0;
+		Vmove = -Time.deltaTime;
+		overTime = Time.time + delay;
 	}
 }
