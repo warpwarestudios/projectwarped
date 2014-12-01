b6665efb45e03f9ab0ac05e1fc192c293ca86b93
Ryan Hammontree <HammontreeRyan@gmail.com>
2014-12-01 17:49:36 -0600

    more directional bools

--- projectwarped/Assets/Scripts/Character Branch/PlayerController.cs
+++ projectwarped/Assets/Scripts/Character Branch/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController: MonoBehaviour {
 
 	// standard movement values
 	public float maxSpeed = 10f;
-	public bool facingRight = false;
+	public bool fliped = true;
+	public bool facingRight = true;
+	public bool facingLeft = false;
 	public bool facingUp = false;
 	public bool facingDown = false;
 
@@ -42,28 +44,55 @@ public class PlayerController: MonoBehaviour {
 		//animator.SetFloat ("Speed", Mathf.Abs (move));
 		
 		//Flips Sprite
-		if (Hmove > 0 && !facingRight) 
+		if (Hmove > 0 && !fliped) 
 			Flip ();
-		else if (Hmove < 0 && facingRight) 
+		else if (Hmove < 0 && fliped) 
 			Flip ();
 
-		// confirms vertical direction
+		// confirms direction
+		if (Hmove > 0 && !facingRight) 
+			facingRight = !facingRight;
+		else if (Hmove < 0 && facingRight)
+			facingRight = !facingRight;
+
+		if (Hmove < 0 && !facingLeft) 
+			facingLeft = !facingLeft;
+		else if (Hmove > 0 && facingLeft)
+			facingLeft = !facingLeft;
+
 		if (Vmove > 0 && !facingUp) 
+		{
+			facingRight = false;
+			facingLeft = false;
 			facingUp = !facingUp;
+		}	
 		else if (Vmove < 0 && facingUp)
+		{
+			facingRight = false;
+			facingLeft = false;
 			facingUp = !facingUp;
+		}
+			
 
 		if (Vmove > 0 && facingDown) 
+		{
+			facingRight = false;
+			facingLeft = false;
 			facingDown = !facingDown;
+		}
 		else if (Vmove < 0 && !facingDown)
+		{
+			facingRight = false;
+			facingLeft = false;
 			facingDown = !facingDown;
+		}
 		
 	}
 	
 	// Flips Sprite
 	void Flip()
 	{
-		facingRight = !facingRight;
+		fliped = !fliped;
 		Vector2 scale = transform.localScale;
 		scale.x *= -1;
 		transform.localScale = scale;
