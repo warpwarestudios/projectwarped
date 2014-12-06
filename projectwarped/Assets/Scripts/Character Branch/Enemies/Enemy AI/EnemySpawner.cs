255785ecb82df3c46667557afa52fcd902539a44
Kenneth Smith <kwgsmith@gmail.com>
2014-12-06 10:34:37 -0600

    Randomized enemy spawn location
    Enemies now spawn all at once, but in a circle a random distance from
    the player. Once all enemies are killed enemies will respawn.

--- projectwarped/Assets/Scripts/Character Branch/Enemies/Enemy AI/EnemySpawner.cs
+++ projectwarped/Assets/Scripts/Character Branch/Enemies/Enemy AI/EnemySpawner.cs
@@ -6,6 +6,8 @@ public class EnemySpawner : MonoBehaviour {
 	public string enemyName;
 	public float timeToSpawn = 1.0f;
 	public int maxNumberOfEnemies = 1;
+	public int randomRangeMin = 2;
+	public int randomRangeMax = 3;
 
 	private GameObject enemy;
 	private GameObject hudText;
@@ -30,7 +32,7 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (GameObject.FindGameObjectsWithTag ("Enemy").Length < maxNumberOfEnemies) 
+		if (GameObject.FindGameObjectsWithTag ("Enemy").Length == 0) 
 		{
 			Spawn ();
 		}
@@ -38,26 +40,32 @@ public class EnemySpawner : MonoBehaviour {
 
 	private void Spawn()
 	{	
-		//TODO: Instantiate with Photon Networking stuff
-		//Instantiate Enemy
-		GameObject newEnemy = Instantiate (enemy) as GameObject;
-		//Instantiate HUD Text
-		GameObject hud = Instantiate (hudText) as GameObject;
-		//hud.transform.parent = uiRoot.transform;
-		//Following script
-		follow = hud.GetComponent<UIFollowTarget>();
-	//	enemyController = newEnemy.GetComponent<EnemyController>();
-	//	enemyController.movePattern = enemyController.movePatternArray [Random.Range (0f, float.Parse(enemyController.movePatternArray.GetLength()))];
-		//hook up HUDText to follow this enemy
-		follow.target = newEnemy.transform.FindChild("Target").transform;
-		//attach cameras to HUDText
-		follow.gameCamera = GameObject.FindGameObjectWithTag("MainCamera").camera;
-		follow.uiCamera = uiRoot.transform.FindChild ("Camera").camera;
-		//add hudText to enemy
-		Health eHealth = newEnemy.GetComponent<Health> ();
-		eHealth.floatText = hud.GetComponent<HUDText>();
-		//set healthbar to track health
-		HealthBar eHealthBar = hud.GetComponentInChildren<HealthBar> ();
-		eHealthBar.health = eHealth;
+		for (int i = 0; i < maxNumberOfEnemies; i++) 
+		{
+			//TODO: Instantiate with Photon Networking stuff
+			//Instantiate Enemy at random range and different angle
+			var randomRange = Random.Range (randomRangeMin, randomRangeMax);
+			var angle = i * Mathf.PI * 2 / maxNumberOfEnemies;
+			Vector3 pos = this.transform.position + new Vector3 (Mathf.Cos (angle), Mathf.Sin (angle), 0) * randomRange;
+			GameObject newEnemy = Instantiate (enemy, pos, Quaternion.identity) as GameObject;
+			//Instantiate HUD Text
+			GameObject hud = Instantiate (hudText) as GameObject;
+			//hud.transform.parent = uiRoot.transform;
+			//Following script
+			follow = hud.GetComponent<UIFollowTarget> ();
+			//	enemyController = newEnemy.GetComponent<EnemyController>();
+			//	enemyController.movePattern = enemyController.movePatternArray [Random.Range (0f, float.Parse(enemyController.movePatternArray.GetLength()))];
+			//hook up HUDText to follow this enemy
+			follow.target = newEnemy.transform.FindChild ("Target").transform;
+			//attach cameras to HUDText
+			follow.gameCamera = GameObject.FindGameObjectWithTag ("MainCamera").camera;
+			follow.uiCamera = uiRoot.transform.FindChild ("Camera").camera;
+			//add hudText to enemy
+			Health eHealth = newEnemy.GetComponent<Health> ();
+			eHealth.floatText = hud.GetComponent<HUDText> ();
+			//set healthbar to track health
+			HealthBar eHealthBar = hud.GetComponentInChildren<HealthBar> ();
+			eHealthBar.health = eHealth;
+		}
 	}
 }
